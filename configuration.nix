738d4c65c553d1e83937e5457e449d6c9d855bff
g.sastry@gmail.com <girish@Girishs-MacBook-Pro.local>
2025-08-11 11:40:51 -0600

    Remove description field from configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,37 +1,61 @@
-# flake.nix - Fixed for DigitalOcean deployment
-{
-  description = "NixOS configuration for DigitalOcean droplet";
+{ config, pkgs, ... }:
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+{
+  # Time zone and locale
+  time.timeZone = "UTC";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Network configuration
+  networking.hostName = "nixos-do";
+  networking.useDHCP = false;
+  networking.interfaces.eth0.useDHCP = true;
+  networking.interfaces.eth1.useDHCP = true; # DO private network
+
+  # Firewall
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 22 80 443 ];
   };
 
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.droplet = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        {
-          # DigitalOcean specific optimizations for small droplets
-          # Enable cloud-init for userdata
-          services.cloud-init.enable = true;
-          
-          # Optimize for small memory
-          nix.settings.max-jobs = 1;
-          nix.settings.cores = 1;
-          
-          # Basic hardware config for DO (since we're not using hardware-configuration.nix)
-          boot.loader.grub.enable = true;
-          boot.loader.grub.device = "/dev/vda";
-          boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
-          boot.initrd.kernelModules = [ "nvme" ];
-          
-          fileSystems."/" = { 
-            device = "/dev/vda1"; 
-            fsType = "ext4"; 
-          };
-        }
-      ];
+  # SSH configuration
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "prohibit-password";
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
     };
   };
+
+  # Your SSH key
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDiiBh8q76NIR1DpjFuCe+Odru4pxnT1onaWmQbStOj/CPec12Bi28K6d04RgPOqDQBNatEJA2ducrO6jBMI3n9rekcN276AyY0edXp/5DDi/qr4LircMRm6Cvdv1ynWZqG24xCTUy0Hi1ngiOXjphOo73ayf1Uoe/5JTqThKvruzyDlmzjZsvoW+6XB4r2X9yTbClRnGyIf8+2nXo/003pOy78+nkSkhJgeh+/IkDtiDKhg540So1jMwwlNYNvWuE1j5EKJBEa8Pfp4bQbT7TntKElAo/jHlkWufRgk3jML3sTirIXWp2mft4Nrjr0WV/5ASHIYqxmWwjnesxzccmuYGum/mLChoo4Jp/QTgqe8BC/RAN0RAdbWcssuPm7Aqakb/uYxa9s5hUU22pEdGzkeA6QcD94QAgXlECSDQ2aGXCiEUvsZgHZmFKGAQ77l7GLU9uhb9GzMlXEEjHol1VeR0shcrF/uLotyTdbmZQ7OaMDRUzJS48QtMVMmj6jEHcP/BR9KRJzyryPbhPX0UyIkAKKvMyCpFmarHytCt2pghTmuIRYtCeD+bAhbDEf9sfBFUP5MDLlvZa0TNDyjMfaWeXrE9rqlKDnvLK9zV5FiQfbV1WrviTPgzbkD3P1A8I/iHZCzKjUO6Ll1wVQ6eTTqSppj14u7pzyQN8mMRo4oQ== g.sastry@gmail.com"
+  ];
+
+  # Essential packages
+  environment.systemPackages = with pkgs; [
+    vim wget curl git htop tmux tree unzip rsync
+  ];
+
+  # Nix settings
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Auto cleanup to save space
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
+  # Optimize for small memory systems
+  services.journald.extraConfig = ''
+    SystemMaxUse=50M
+    RuntimeMaxUse=10M
+  '';
+
+  # Disable unnecessary services for small droplets
+  services.udisks2.enable = false;
+  security.polkit.enable = false;
+
+  system.stateVersion = "25.05";
 }
